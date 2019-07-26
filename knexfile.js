1b351297a577eb41b0d1e1d43972a650f68888d2
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-07-26 13:39:57 +0400

    setup: to test with dependencies, server test, knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,31 @@
 
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: { filename: './database/auth.db3' }, // change this if you want a different name for the database
+    client: "sqlite3",
+    connection: { filename: "./database/auth.db3" }, // change this if you want a different name for the database
     useNullAsDefault: true, // used to avoid warning on console
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+      directory: "./database/migrations",
+      tableName: "dbmigrations"
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { directory: "./database/seeds" }
   },
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./database/test.db3"
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_key=ON", done);
+      }
+    },
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  }
 };
