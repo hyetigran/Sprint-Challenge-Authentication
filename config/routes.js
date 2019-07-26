c8c4a76f8a70b8b4d47c325493cf9e2fdeae56c7
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-07-26 12:08:20 +0400

    add: register route

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,34 @@
-const axios = require('axios');
-
-const { authenticate } = require('../auth/authenticate');
+const axios = require("axios");
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { authenticate } = require("../auth/authenticate");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
-function register(req, res) {
-  // implement user registration
+async function register(req, res) {
+  try {
+    const user = req.body;
+    const { username } = req.body;
+    user.password = bcrypt.hashSync(user.password, 12);
+    const userExists = await findBy({ username });
+    if (userExists) {
+      res.status(409).json({
+        message: "User already exists"
+      });
+    } else {
+      const successfulRegister = await add(user);
+      res.status(201).json({ successfulRegister });
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: "Internal error, please try again later"
+    });
+  }
 }
 
 function login(req, res) {
@@ -18,15 +37,15 @@ function login(req, res) {
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
